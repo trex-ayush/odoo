c92e2f6a0cf7a4e01889929dfc5c79d769d73336
Nishant Singh <nishu55647@gmail.com>
2025-07-12 14:28:19 +0530

    updated auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { signup, login } = require("../controller/auth");
+const { signup, login, logout } = require("../controller/auth");
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/logout", logout);
 
 module.exports = router;
